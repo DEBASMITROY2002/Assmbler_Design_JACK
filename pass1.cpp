0ec4a0b9b3ae61ec85b4c6e61e252e0a367d14ed
DEBASMITROY2002 <ddruk2018@gmail.com>
2022-11-02 19:23:39 +0530

    pass1 started

---
+++ pass1.cpp
@@ -0,0 +1,21 @@
+/*
+Authors:
+1. Debasmit Roy, JU CSE
+2. Anurup Naskar,JU CSE
+3. Imon Raj, JU CSE
+4. Kushal Das, JU CSE
+
+*/
+
+#include <iostream>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    FILE *fp = fopen("./Max.asm","r");
+    
+    fclose(fp);
+}
